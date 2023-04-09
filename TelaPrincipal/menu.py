80abfc35c279ccd9194a5bc4e7135b1a5ede68d7
annyfiorese <108233880+annyfiorese@users.noreply.github.com>
2023-04-09 19:40:33 -0300

    Melhorias
    Versão 2 - foi realizado algumas melhorias e ajustes, e implementado a funcionalidade de voltar ao menu principal

--- TelaPrincipal/menu.py
+++ TelaPrincipal/menu.py
@@ -1,39 +1,58 @@
 lista_estudante = []
-print("Olá sejá bem vindo ao nosso sistema academico\n\n")
-print("1 - Cadastro de Estudante\n")
-print("2 - Professor\n")
-print("3 - Diciplinas\n")
-print("4 - Turma\n")
-print("5 - Matricula\n")
-print("6 - Menu de operações\n")
-opcao = int(input(print("Escolha uma opção")))
-if opcao == 1:
-    alunos = True
-    while alunos :
-        aluno = input(print("Nome do Estudante: "))
-        print("\n")
-        print("\n")
-        lista_estudante.append(aluno)
-        print("1 - Novo aluno\n")
-        opcao = int(input(print("2 - Listar Estudantes\n ")))
-        if opcao == 2:
-            alunos = False 
-            for listar_estudante in lista_estudante:
-                print(listar_estudante)
-if opcao == 2:
-    print("EM DESENVOLVIMENTO")    
 
-if opcao == 3:
-    print("EM DESENVOLVIMENTO")        
+while True:
+    print("\nOlá, seja bem-vindo ao nosso sistema acadêmico\n")
+    print("Escolha uma opção:")
+    print("1 - Cadastro de Estudante")
+    print("2 - Professor")
+    print("3 - Disciplinas")
+    print("4 - Turma")
+    print("5 - Matrícula")
+    print("6 - Menu de operações")
+    print("0 - Sair do programa")
 
-if opcao == 4:
-    print("EM DESENVOLVIMENTO")
+    opcao = int(input())
 
-if opcao == 5:
-    print("EM DESENVOLVIMENTO")
+    if opcao == 1:
+        while True:
+            aluno = input("Nome do Estudante: ")
+            lista_estudante.append(aluno)
+            print("1 - Novo aluno")
+            print("2 - Listar Estudantes")
+            print("0 - Voltar ao menu principal")
+            opcao = int(input())
+            if opcao == 0:
+                break
+                
+        for listar_estudante in lista_estudante:
+            print(listar_estudante)
 
-if opcao == 6:
-    print("1 - Atualizar\n")
-    print("2 - Excluir\n")
-    op = input() 
-    print("EM DESENVOLVIMENTO")
+    elif opcao == 2:
+        print("EM DESENVOLVIMENTO")
+        
+    elif opcao == 3:
+        print("EM DESENVOLVIMENTO")
+        
+    elif opcao == 4:
+        print("EM DESENVOLVIMENTO")
+        
+    elif opcao == 5:
+        print("EM DESENVOLVIMENTO")
+        
+    elif opcao == 6:
+        while True:
+            print("Escolha uma opção:")
+            print("1 - Atualizar")
+            print("2 - Excluir")
+            print("0 - Voltar ao menu principal")
+            op = input() 
+            if op == "0":
+                break
+            print("EM DESENVOLVIMENTO")
+        
+    elif opcao == 0:
+        print("Saindo do programa...")
+        break
+        
+    else:
+        print("Opção inválida. Tente novamente.")
